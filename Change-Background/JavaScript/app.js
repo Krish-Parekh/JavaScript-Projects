c7b1a75a27cc6b8c7985b2491b3372a7c76f5d98
Krish-Parekh <73629899+Krish-Parekh@users.noreply.github.com>
2020-12-31 13:12:24 +0530

    Add files via upload

--- Change-Background/JavaScript/app.js
+++ Change-Background/JavaScript/app.js
@@ -1,1 +1,12 @@
+const background = document.querySelector('section');
+const button = document.querySelector('button');
+const heading = document.querySelector('h1');
 
+button.addEventListener('click',()=>{
+    let color = '#';
+    color+= Math.random().toString(16).slice(2,8);
+    background.style.backgroundColor = color;
+    button.style.backgroundColor = color;
+    heading.innerText = color;
+    console.log(color);
+});
